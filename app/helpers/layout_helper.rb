d5e1dbc12b46ab76e19bfc5f50b0409759ead8b6
David Chun <daveedchun@gmail.com>
2014-05-13 15:37:36 +0700

    helper method for admin sidebar nav

--- app/helpers/layout_helper.rb
+++ app/helpers/layout_helper.rb
@@ -12,4 +12,32 @@ module LayoutHelper
   def show_layout_flash?
     @layout_flash.nil ? true : @layout_flash
   end
+
+  def sidebar_nav(link_path, link_text, link_icon)
+    li_class = current_page?(link_path) ? 'active' : ''
+
+    content_tag(:li, class: li_class) do
+      link_to link_path do
+        content_tag(:i, "", class: link_icon) +
+        content_tag(:span, link_text)
+      end
+    end
+  end
+
+  def submenu_nav(link_path, link_text, link_icon, submenu={})
+    li_class = current_page?(link_path) ? 'active' : ''
+
+    content_tag(:li, class: li_class) do
+      content = link_to link_path, class: 'dropdown-toggle' do
+        content_tag(:i, "", class: link_icon) +
+        content_tag(:span, link_text) +
+        content_tag(:i, "", class: "fa fa-chevron-circle-down drop-icon")
+      end
+      content += content_tag(:ul, class: 'submenu') do
+        submenu.each do |k, v|
+          concat content_tag(:li, (link_to k, v))
+        end
+      end
+    end
+  end
 end
