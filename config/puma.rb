dbf945fa519d9c51a9c5b166ad0d19086c26d23c
sir1001dem <sir1001dem@gmail.com>
2015-01-29 17:54:00 +0700

    puma config

--- config/puma.rb
+++ config/puma.rb
@@ -1,2 +1,9 @@
 environment ENV['RACK_ENV']
 threads 0,5
+
+on_worker_boot do
+  ActiveSupport.on_load(:active_record) do
+    config = ActiveRecord::Base.configurations[Rails.env] || Rails.application.config.database_configuration[Rails.env]
+    ActiveRecord::Base.establish_connection(config)
+  end
+end
