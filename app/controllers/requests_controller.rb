27f3d1356d6158def197197a71c506f5c5211ca7
sir1001dem <sir1001dem@gmail.com>
2015-02-02 17:38:34 +0700

    File field completed

--- app/controllers/requests_controller.rb
+++ app/controllers/requests_controller.rb
@@ -1,7 +1,7 @@
 class RequestsController < ApplicationController
   before_filter :authenticate_user!, except: [:create]
   before_action :set_request, only: [:show, :destroy]
-  
+
   skip_before_filter :verify_authenticity_token
   after_filter  :add_origin_header
 
@@ -27,32 +27,43 @@ class RequestsController < ApplicationController
   def create
     req_params = request_params
     form = Form.find(req_params[:form_id].to_i)
-    @request = Request.new(req_params)
-    name = params[:request][:fields].find{|k,v| v['type'] == 'name'}
-    phone = params[:request][:fields].find{|k,v| v['type'] == 'phone'}
-    email = email.last['request'] if email.present?
-    name = name.last['request'] if name.present?
-    phone = phone.last['request'] if phone.present?
+
+    req_params = store_file(req_params)
     back = request.referrer
 
-    if email.present?
-      contact = Contact.find_by_email(email) || Contact.new
-      contact.email = email if contact.email.blank?
-      contact.name = name if contact.name.blank?
-      contact.phone = phone if contact.phone.blank?
-      contact.save
-      @request.contact = contact
-    end
+    if req_params[:errors].empty?
+      @request = Request.new(req_params[:params])
 
-    # Check and get errors list
-    errors = form.map_destination_data(req_params)
+      name = req_params[:params][:fields].find{|k,v| v['type'] == 'name'}
+      phone = req_params[:params][:fields].find{|k,v| v['type'] == 'phone'}
+      email = email.last['request'] if email.present?
+      name = name.last['request'] if name.present?
+      phone = phone.last['request'] if phone.present?
 
-    respond_to do |format|
-      if errors.messages.size == 0 && @request.save
-        format.html { redirect_to back, notice: 'Request was successfully created' }
-        format.json { render json: @request.to_json, status: :created }
-      else
-        format.html { redirect_to back, notice: 'Please enter required fields' }
+      if email.present?
+        contact = Contact.find_by_email(email) || Contact.new
+        contact.email = email if contact.email.blank?
+        contact.name = name if contact.name.blank?
+        contact.phone = phone if contact.phone.blank?
+        contact.save
+        @request.contact = contact
+      end
+
+      # Check and get errors list
+      errors = form.map_destination_data(req_params[:params])
+
+      respond_to do |format|
+        if errors.messages.size == 0 && @request.save
+          format.html { redirect_to back, notice: 'Request was successfully created' }
+          format.json { render json: @request.to_json, status: :created }
+        else
+          format.html { redirect_to back, alert: 'Input Fields are invalid' }
+          format.json { render json: errors, status: :unprocessable_entity }
+        end
+      end
+    else 
+      respond_to do |format|
+        format.html { redirect_to back, alert: 'Invalid form fields' }
         format.json { render json: errors, status: :unprocessable_entity }
       end
     end
@@ -69,15 +80,42 @@ class RequestsController < ApplicationController
   end
 
   private
-    # Use callbacks to share common setup or constraints between actions.
-    def set_request
-      @request = Request.find(params[:id])
-    end
+  # Use callbacks to share common setup or constraints between actions.
+  def set_request
+    @request = Request.find(params[:id])
+  end
 
-    # Never trust parameters from the scary internet, only allow the white list through.
-    def request_params
-      params.require(:request).permit(:form_id).tap do |whitelisted|
-        whitelisted[:fields] = params[:request][:fields]
-      end    
+  # Never trust parameters from the scary internet, only allow the white list through.
+  def request_params
+    params.require(:request).permit(:form_id).tap do |whitelisted|
+      whitelisted[:fields] = params[:request][:fields]
+    end    
+  end
+
+  def store_file(params)
+    errors = {}
+
+    params['fields'].each do |k, v| 
+      if v['type'] == 'file'
+        unless v['request'].nil?
+          isValidFile = ['png', 'jpg', 'jpeg', 'gif', 'zip'].any? do |word| 
+            v['request'].content_type.include?(word)
+          end
+
+          if !isValidFile
+            errors[:file_extensions] = 'Only allow to add [jpg, png, gif, zip] file'
+          end 
+
+          if (v['request'].tempfile.size / 1000000 > 2) 
+            errors[:file_size] = 'The file size exceeds the limit allowed and cannot be saved'
+          else 
+            uploader = FileUploader.new 
+            uploader.cache!(v['request'])
+            v['request'] = uploader.url
+          end
+        end
+      end
     end
+    { :params => params, :errors => errors }
+  end
 end
